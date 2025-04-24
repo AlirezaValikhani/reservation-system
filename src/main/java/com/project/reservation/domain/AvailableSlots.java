d25bc8030713d683ea84b45ebd757638c41de21f
vk <alirezavalikhani1998@gmail.com>
2025-04-24 11:42:52 +0330

    feat: Add all models

---
+++ src/main/java/com/project/reservation/domain/AvailableSlots.java
@@ -0,0 +1,31 @@
+package com.project.reservation.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "available_slots", schema = "new")
+public class AvailableSlots {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(name = "is_reserved", nullable = false)
+    private Boolean isReserved;
+}
