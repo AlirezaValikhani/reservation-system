1ffff0565b780a477349024af05168fbc7cecbb5
vk <alirezavalikhani1998@gmail.com>
2025-04-26 14:49:29 +0330

    feat/alivk-20250425-add-services-and-implementation-of-services

--- src/main/java/com/project/reservation/service/impl/AvailableSlotsServiceImpl.java
+++ src/main/java/com/project/reservation/service/impl/AvailableSlotsServiceImpl.java
@@ -1,6 +1,10 @@
 package com.project.reservation.service.impl;
 
 import com.project.reservation.service.AvailableSlotsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AvailableSlotsServiceImpl implements AvailableSlotsService {
 }
---
+++ src/main/java/com/project/reservation/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.project.reservation.service.impl;
+
+import com.project.reservation.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
